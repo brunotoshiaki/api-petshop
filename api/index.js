050f0992f78242f54cdb262baeb7b4985e01f10c
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-11-13 19:12:45 -0300

    Criando Rota Fornecedor

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
-const config = require('config')
-
+const config = require('config');
+const roteador = require('./rotas/fornecedores')
 
 app.use(express.json());
+
+
+
+app.use('/api/fornecedores', roteador)
 
 
 app.listen(config.get('api.porta'), () => console.log('A API está funcionando!'))
