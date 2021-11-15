f2b60d4aa9aa8c1505b707bbd1c43a49b58d1a81
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-11-15 09:26:53 -0300

    Adicinando validação de header

--- api/index.js
+++ api/index.js
@@ -3,13 +3,36 @@ const app = express()
 const config = require('config');
 const roteador = require('./rotas/fornecedores')
 const NotFound = require('./erros/NotFound')
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const formatosAceitos = require('./Serializador').formatosAceitos
 
 app.use(express.json());
+
+app.use((requisicao, resposta, proximo) => {
+    let formatoRequisitado = requisicao.header('Accept')
+    if (formatoRequisitado === '*/*') {
+        formatoRequisitado = 'application/json'
+    }
+    if (formatosAceitos.indexOf(formatoRequisitado) === -1) {
+        resposta.status(406)
+        resposta.end()
+        return
+    }
+
+    resposta.setHeader('Content-Type', formatoRequisitado)
+    proximo()
+
+})
+
 app.use('/api/fornecedores', roteador)
 app.use((erro, requisicao, resposta, proximo) => {
     let status = 500
     if (erro instanceof NotFound) {
         status = 404
+    }
+
+    if (erro instanceof ValorNaoSuportado) {
+        status = 406
     } else {
         status = 400
     }
