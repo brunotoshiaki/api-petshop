09da81cfa59817ea2b9018e648448ddadc1c502b
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-11-15 11:20:53 -0300

    Adicionando xml

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,15 +1,39 @@
+const jsontoxml = require('jsontoxml')
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+
+
 class Serializador {
     json(dados) {
         return JSON.stringify(dados)
     }
+    xml(dados) {
+        let tag = this.tagSingular
+
+        if (Array.isArray(dados)) {
+            tag = this.tagPlural
+            dados = dados.map((item) => {
+                return {
+                    [this.tagSingular]: item
+                }
+            })
+        }
+
+        return jsontoxml({
+            [tag]: dados
+        })
+    }
 
     serializar(dados) {
+        dados = this.filtrar(dados)
+
         if (this.contentType === 'application/json') {
-            return this.json(
-                this.filtrar(dados)
-            )
+            return this.json(dados)
         }
+
+        if (this.contentType === 'application/xml') {
+            return this.xml(dados)
+        }
+
         throw new ValorNaoSuportado(this.contentType)
     }
 
@@ -41,6 +65,8 @@ class SerializadorFornecedor extends Serializador {
         super()
         this.contentType = contentType
         this.camposPublicos = ['id', 'empresa', 'categoria'].concat(camposExtras || [])
+        this.tagSingular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
     }
 }
 
@@ -49,6 +75,9 @@ class SerializadorErro extends Serializador {
         super()
         this.contentType = contentType
         this.camposPublicos = ['id', 'mensagem'].concat(camposExtras || [])
+        this.tagSingular = 'erro'
+        this.tagPlural = 'erros'
+
     }
 }
 
@@ -56,5 +85,5 @@ module.exports = {
     Serializador: Serializador,
     SerializadorFornecedor: SerializadorFornecedor,
     SerializadorErro: SerializadorErro,
-    formatosAceitos: ['application/json']
+    formatosAceitos: ['application/json', 'application/xml']
 }
