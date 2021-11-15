521ffedd3d5ccdcdcbc745a2deb2d44312e9d770
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2021-11-15 10:10:56 -0300

    Adicionando Serialização

--- api/Serializador.js
+++ api/Serializador.js
@@ -11,7 +11,16 @@ class Serializador {
         throw new ValorNaoSuportado(this.contentType)
     }
 }
+
+class SerializadorFornecedor extends Serializador {
+    constructor(contentType) {
+        super()
+        this.contentType = contentType
+    }
+}
+
 module.exports = {
     Serializador: Serializador,
+    SerializadorFornecedor: SerializadorFornecedor,
     formatosAceitos: ['application/json']
 }
